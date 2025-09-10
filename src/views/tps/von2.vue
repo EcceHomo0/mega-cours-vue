1fb568a7225aa63b36a7570e7b591029951db2bc
Yann <yanntieba@gmail.com>
2025-09-10 13:40:42 +0200

    rectification tp v-on 2

--- src/views/tps/von2.vue
+++ src/views/tps/von2.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-base-100 shadow-sm">
     <h1>TP Event Binding</h1>
-    <input type="text" v-model="inputValue"/>
+    <input type=text v-on:keyup="afficherInput($event)" />
     <p>Le contenu de l'input 1 est: {{ inputValue }}</p>
-    <input type="text" v-model="inputValue2" v-on:keydown.esc="updateDisplayValue2" />
-    <p>Le contenu de l'input 2 est: {{ displayValue2 }}</p>
+    <input type="text" @keyup.esc="InputConfirm($event)" />
+    <p>Le contenu de l'input 2 est: {{ inputValue2 }}</p>
     <button class="btn btn-primary" v-on:click="alerte">Cliquez ici</button>
   </div>
 </template>
@@ -14,10 +14,14 @@ import { ref } from 'vue'
 
 const inputValue = ref('');
 const inputValue2 = ref('');
-const displayValue2 = ref('');
 
-const updateDisplayValue2 = () => {
-  displayValue2.value = inputValue2.value;
+function afficherInput(event){
+  console.log(event);
+  inputValue.value = event.target.value;
+}
+
+function InputConfirm(event) {
+  inputValue2.value = event.target.value;
 }
 
 const alerte = () => {
@@ -25,6 +29,7 @@ const alerte = () => {
 }
 
 </script>
+
 
 
 
